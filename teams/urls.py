16c02e9d68fe6afd0477bfe97a83439b7f4bf677
Danchik <danisaifudinov@gmail.com>
2026-01-12 12:19:31 +0600

    Fix teams urls - remove .as_view() from function-based views

--- teams/urls.py
+++ teams/urls.py
@@ -1,3 +1,4 @@
+# teams/urls.py
 from django.urls import path
 from . import views
 
@@ -8,8 +9,10 @@ urlpatterns = [
     path('create/', views.TeamCreateView.as_view(), name='create'),
     path('<int:pk>/', views.TeamDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.TeamUpdateView.as_view(), name='edit'),
-    path('<int:pk>/join/', views.TeamJoinView.as_view(), name='join'),
-    path('<int:pk>/leave/', views.TeamLeaveView.as_view(), name='leave'),
-    path('invite/<int:invitation_id>/accept/', views.InvitationAcceptView.as_view(), name='accept_invitation'),
-    path('invite/<int:invitation_id>/decline/', views.InvitationDeclineView.as_view(), name='decline_invitation'),
+    
+    # УБРАТЬ .as_view() для этих функций:
+    path('<int:pk>/join/', views.TeamJoinView, name='join'),
+    path('<int:pk>/leave/', views.TeamLeaveView, name='leave'),
+    path('invite/<int:invitation_id>/accept/', views.InvitationAcceptView, name='accept_invitation'),
+    path('invite/<int:invitation_id>/decline/', views.InvitationDeclineView, name='decline_invitation'),
 ]
